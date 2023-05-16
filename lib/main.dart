3427d673ba705483808d2efac076a292a4dcfd77
geethu-gopan <geethuavanthika2432@gmail.com>
2023-05-16 15:03:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(Center(child:Text('maitexa',textDirection: TextDirection.ltr,style: TextStyle(color: Colors.blue,fontSize: 35,fontStyle: FontStyle.italic))));
+}
